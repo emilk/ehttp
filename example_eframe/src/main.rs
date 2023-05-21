8b8eb37412215ed0f8ac953837ab5c5401346635
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-05-21 10:31:48 +0200

    Update example to eframe 0.21

--- example_eframe/src/main.rs
+++ example_eframe/src/main.rs
@@ -1,7 +1,7 @@
-fn main() {
+fn main() -> eframe::Result<()> {
     eframe::run_native(
         "ehttp demo",
         Default::default(),
         Box::new(|_cc| Box::new(example_eframe::DemoApp::default())),
-    );
+    )
 }
