b135949698984af12eb08cc59da26089c1171cbd
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-06-13 10:46:50 +0200

    Update egui/eframe to 0.22 (#27)
    * Update egui/eframe to 0.22
    
    * Update deny.toml

--- example_eframe/src/lib.rs
+++ example_eframe/src/lib.rs
@@ -5,20 +5,7 @@ pub use app::DemoApp;
 // ----------------------------------------------------------------------------
 
 #[cfg(target_arch = "wasm32")]
-use eframe::wasm_bindgen::{self, prelude::*};
+mod web;
 
-/// This is the entry-point for all the web-assembly.
-/// This is called once from the HTML.
-/// It loads the app, installs some callbacks, then returns.
-/// You can add more callbacks like this if you want to call in to your code.
 #[cfg(target_arch = "wasm32")]
-#[wasm_bindgen]
-pub async fn start(canvas_id: &str) -> Result<(), wasm_bindgen::JsValue> {
-    eframe::start_web(
-        canvas_id,
-        Default::default(),
-        Box::new(|_cc| Box::new(DemoApp::default())),
-    )
-    .await?;
-    Ok(())
-}
+pub use web::*;
---
+++ example_eframe/src/web.rs
@@ -0,0 +1,58 @@
+use eframe::wasm_bindgen::{self, prelude::*};
+
+use crate::DemoApp;
+
+/// Our handle to the web app from JavaScript.
+#[derive(Clone)]
+#[wasm_bindgen]
+pub struct WebHandle {
+    runner: eframe::WebRunner,
+}
+
+#[wasm_bindgen]
+impl WebHandle {
+    /// Installs a panic hook, then returns.
+    #[allow(clippy::new_without_default)]
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        // Redirect [`log`] message to `console.log` and friends:
+        eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+        Self {
+            runner: eframe::WebRunner::new(),
+        }
+    }
+
+    /// Call this once from JavaScript to start your app.
+    #[wasm_bindgen]
+    pub async fn start(&self, canvas_id: &str) -> Result<(), wasm_bindgen::JsValue> {
+        self.runner
+            .start(
+                canvas_id,
+                eframe::WebOptions::default(),
+                Box::new(|_cc| Box::new(DemoApp::default())),
+            )
+            .await
+    }
+
+    #[wasm_bindgen]
+    pub fn destroy(&self) {
+        self.runner.destroy();
+    }
+
+    /// The JavaScript can check whether or not your app has crashed:
+    #[wasm_bindgen]
+    pub fn has_panicked(&self) -> bool {
+        self.runner.has_panicked()
+    }
+
+    #[wasm_bindgen]
+    pub fn panic_message(&self) -> Option<String> {
+        self.runner.panic_summary().map(|s| s.message())
+    }
+
+    #[wasm_bindgen]
+    pub fn panic_callstack(&self) -> Option<String> {
+        self.runner.panic_summary().map(|s| s.callstack())
+    }
+}
