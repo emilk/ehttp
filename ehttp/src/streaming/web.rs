e2fa26ea80af3d07dc72b8172c457fb83d7b59e7
Jan Procházka <1665677+jprochazk@users.noreply.github.com>
2023-09-12 14:46:50 +0200

    Web: Improve opaque network error message (#33)
    * catch `TypeError` and simplify message
    
    * `fetch_error_to_string` -> `string_from_fetch_error`
    
    * casing
    
    ---------
    
    Co-authored-by: Emil Ernerfeldt <emil.ernerfeldt@gmail.com>

--- ehttp/src/streaming/web.rs
+++ ehttp/src/streaming/web.rs
@@ -4,7 +4,7 @@ use futures_util::Stream;
 use futures_util::StreamExt;
 use wasm_bindgen::prelude::*;
 
-use crate::web::{fetch_base, get_response_base, spawn_future, string_from_js_value};
+use crate::web::{fetch_base, get_response_base, spawn_future, string_from_fetch_error};
 use crate::Request;
 
 use super::types::Part;
@@ -19,8 +19,8 @@ pub async fn fetch_async_streaming(
 ) -> crate::Result<impl Stream<Item = crate::Result<Part>>> {
     let stream = fetch_jsvalue_stream(request)
         .await
-        .map_err(string_from_js_value)?;
-    Ok(stream.map(|result| result.map_err(string_from_js_value)))
+        .map_err(string_from_fetch_error)?;
+    Ok(stream.map(|result| result.map_err(string_from_fetch_error)))
 }
 
 #[cfg(feature = "streaming")]
@@ -54,7 +54,7 @@ pub(crate) fn fetch_streaming(
         let mut stream = match fetch_jsvalue_stream(&request).await {
             Ok(stream) => stream,
             Err(e) => {
-                on_data(Err(string_from_js_value(e)));
+                on_data(Err(string_from_fetch_error(e)));
                 return;
             }
         };
@@ -67,7 +67,7 @@ pub(crate) fn fetch_streaming(
                     }
                 }
                 Err(e) => {
-                    on_data(Err(string_from_js_value(e)));
+                    on_data(Err(string_from_fetch_error(e)));
                     return;
                 }
             }
