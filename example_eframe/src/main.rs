c9c9eeffd4ebf2cddb8a6437cb3074a040becb52
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-12-31 10:55:37 +0100

    Fix small clippy warning

--- example_eframe/src/main.rs
+++ example_eframe/src/main.rs
@@ -4,6 +4,6 @@ fn main() -> eframe::Result<()> {
     eframe::run_native(
         "ehttp demo",
         Default::default(),
-        Box::new(|_cc| Box::new(example_eframe::DemoApp::default())),
+        Box::new(|_cc| Box::<example_eframe::DemoApp>::default()),
     )
 }
